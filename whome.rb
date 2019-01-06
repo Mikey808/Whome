8b3a8ea67f11214aa9ec9782e06694605dac752c
Mikey808 <18650557+Mikey808@users.noreply.github.com>
2019-01-05 23:37:06 -0600

    Add files via upload

---
+++ whome.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+#--------------------------------------#
+# WhoMe                                #
+# Script to get ip and then do whois   #
+# whome-v2.rb                          # 
+# Auth: Mike Rixford                   #
+#--------------------------------------#
+
+require 'net/http'
+require 'whois'
+
+@ip = Net::HTTP.get(URI("https://api.ipify.org"))
+@whois = Whois.whois(@ip).to_s
+# Some fields only show in specific countries
+@filter = "NetRange:", 
+          "inetnum", 
+          "netname:", 
+          "NetName:",
+          "descr:", 
+          "country", 
+          "address", 
+          "OrgName:", 
+          "City:", 
+          "StateProv:", 
+          "Country:"
+
+puts "IP: #{@ip}"
+puts " "
+
+@whois.each_line do |line|
+  if line.start_with?(*@filter)
+    puts line
+  end
+end
